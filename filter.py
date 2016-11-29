eb3b47df726d59a4d051b859a1e02e6ebca211f9
keligeri <kel.gergo@gmail.com>
2016-11-29 14:02:19 +0100

    Ready

--- filter.py
+++ filter.py
@@ -1,9 +1,16 @@
 # Tulajdonképpen törölhetek vele elemeket, de gyakorlatilag nem töröl, hanem
 # a kért elemeket adja vissza
+"""sdfsdf
+sadfsadf
+s
+adf
+as
+sdfsdf
+dsfsef
+"""
+nums = [x for x in range(0, 101)]
 
-nums = [x for x in range(0, 51)]
 
-
-number = list(filter(lambda x: x % 5 == 0, nums))
+number = list(filter(lambda x: x % 4 == 0, nums))
 
 print (number)
--- map.py
+++ map.py
@@ -1,8 +1,18 @@
 # megadhatok neki egy functiont első paraméterként és egy iterable-t (listát, tuple, dict ..)
 # második paraméterként
+"""sdf
+sdf
+sdf
+setsdf
+sdf
+sdf
+asdf
+sadf
+asdf"""
 
-nums = [x for x in range(10, 100) if x % 11 == 0]
-
+nums = [x for x in range(10, 100) if x % 10 == 0]
+# sdfsadfsdf
 number = list(map(lambda x: x / 2, nums))
 
+# sdfsadfasdfsadfsdf
 print (number)
